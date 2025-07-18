dc6421e6af618044426651d3a3193ee93e96e7cb
Bahaa-Hussein1 <bha304932@gmail.com>
2025-07-18 20:12:24 +0300

    first commit

---
+++ cypress/e2e/taskTwo/addToCompare.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+describe('Add to Compare', () => {
+    it('validate that the user can be Add to Compare', () => {
+        cy.visit("/")
+        cy.get(".product-item-info").first().click()
+        //cy.contains("Add to Compare").should('be.visible').click()
+        cy.get(".action.tocompare").should('be.visible').click()
+        //cy.get("[data-role=add-to-links]").should('be.visible').click()
+
+        cy.wait(8000)
+       
+        cy.get("[role=alert]").should('be.visible').and("contain","You added product Radiant Tee to the comparison list.")
+        
+    });
+    
+});
